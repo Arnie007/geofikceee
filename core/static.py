6da739b78a4c1e5a457839c6ef9451983b365ae3
Arnie007 <75370671+Arnie007@users.noreply.github.com>
2022-01-05 20:16:31 +0100

    cm

---
+++ core/static.py
@@ -0,0 +1,8 @@
+class rarity(object):
+    common = 0xABABAB
+    uncommon = 0x12CC00
+    rare = 0x009DE3
+    epic = 0x8C25FF
+    legendary = 0xFF8F00
+    mythic = 0xFF338AF
+    event = 0xFF0000
